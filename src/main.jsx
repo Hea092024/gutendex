1f9152b90ca85a29d351db2f29c85320216d0fc1
Hector Acevedo <HEA092024@gmail.com>
2025-03-03 10:44:39 +0100

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import BookList from "./components/BookList";
+import Category from "./components/Category";
+import BookDetail from "./components/BookDetail";
+import Favorites from "./components/Favorites";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1>Error</h1>,
+    children: [
+      {
+        path: "/",
+        element: <BookList />,
+      },
+      {
+        path: "/category/:topic",
+        element: <Category />,
+      },
+      {
+        path: "/book/:id",
+        element: <BookDetail />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <App />,
+    errorElement: <h1>404 - Page not found </h1>,
+  },
+]);
+
+document.title = "Hector's Book Haven";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
