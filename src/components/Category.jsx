999cb6993a57ca17785f536fe70732cc9478b64b
Hector Acevedo <HEA092024@gmail.com>
2025-03-03 10:50:33 +0100

    Edited Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom";
+import BookList from "./BookList"; 
+
 export default function Category() {
-    return (
-        <>
-            <h1>Category</h1>
-        </>
-    )
+  const { topic } = useParams();
+
+  return (
+    <div className="category-page">
+      <h2>{topic.charAt(0).toUpperCase() + topic.slice(1)} Books</h2>
+      <BookList searchTerm={topic} />{" "}
+      
+    </div>
+  );
 }
