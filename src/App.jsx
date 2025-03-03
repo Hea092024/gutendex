3a14892abcfa3f5152aa76f4915f5d54d66a980b
Hector Acevedo <HEA092024@gmail.com>
2025-03-03 11:58:56 +0100

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
-function App() {
+export default function App() {
   return (
     <div className="app">
       <Header />
@@ -11,5 +11,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
